b6ae5326dd844fabdb828aa304d7ddf687667313
Rzeeshann <zeeshansk433@gmail.com>
2022-12-10 00:06:04 +0530

    Parsing Incoming Requests

--- WEBDEVELOPMENT/Backend Development/wrap.js
+++ WEBDEVELOPMENT/Backend Development/wrap.js
@@ -1,13 +1,24 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const wrap = express();
 
-wrap.use((req, res, next)=>{
-    console.log('I am in Middleware');
-    next();
-    res.send('<h1>Hello!</h1>');
+wrap.use(bodyParser.urlencoded({extended: false}));
+
+wrap.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type = "text" name="title"><button type="button">Click Me!</button> <h1>Hello!</h1>');
 })
+
+wrap.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+wrap.use('/', (req, res, next)=>{
+    res.send('<h1>Hello zeee</h1>');
+    next();
+})
+
 
 wrap.listen(3000);
 
